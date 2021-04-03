9220215a32185a8b4a2fdba046f3b30e634f4cae
Brendan Chabannes <brendan.chabannes@hey.com>
2021-04-03 09:13:21 +0200

    implement tests

--- pydecharge.py
+++ pydecharge.py
@@ -9,12 +9,39 @@ import os
 from pathlib import Path
 import shutil
 import argparse
-
+import re
 import glob
 
 from openpyxl import Workbook, load_workbook
 
-# def check_row(row):
+
+def check_row(row, file, line):
+    line += 1
+    champs = dict(zip(["civilite", "prenom", "nom", "heures_decharge",
+                       "minutes_decharges", "heures_ORS", "corps", "rne"], row))
+    if not champs['civilite'] in ('M.', 'Mme'):
+        print("Fichier {} : erreur dans le champ Civilité de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if not re.fullmatch(r"""[A-ZÀ-ÖØ-ÞŽŸa-zà-öø-ÿ '-]+[a-zà-öø-ÿ]""", champs['prenom']):
+        print("Fichier {} : erreur dans le champ Prénom de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if not re.fullmatch(r"""[A-ZÀ-ÖØ-ÞŽŸ' -]+[A-ZÀ-ÖØ-ÞŽŸ]""", champs['nom']):
+        print("Fichier {} : erreur dans le champ Nom de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if not re.fullmatch(r'\d{2,3}', champs['corps']):
+         print("Fichier {} : erreur dans le champ Corps de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if not re.fullmatch(r'\d{7}[A-Z]', champs['rne']):
+         print("Fichier {} : erreur dans le champ RNE de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if champs['minutes_decharges'] < 0 or champs['minutes_decharges'] >= 60:
+         print("Fichier {} : erreur dans le champ Minutes de décharge de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    if not champs['heures_ORS'] in (15,18,27,35,36,1607) :
+         print("Fichier {} : erreur dans le champ Heures ORS de la ligne {} traitée.".format(
+            file, line), file=sys.stderr)
+    
+    
 
 
 def save_export_syndicats(export_sheet, output_file_path):
@@ -23,7 +50,7 @@ def save_export_syndicats(export_sheet, output_file_path):
     header = ["Code organisation", "M. Mme", "Prénom", "Nom", "Heures décharges",
               "Minutes décharges", "Heures ORS", "Minutes ORS", "AIRE", "Corps", "RNE"]
     for i in range(len(header)):
-        ws.cell(1,i+1).value = header[i]
+        ws.cell(1, i+1).value = header[i]
     # On remplit le fichier ici avec le contenu de export_sheet
     for row in range(len(export_sheet)):
         export_sheet[row].insert(0, "S01")  # Code organisation → toujours S01
@@ -31,7 +58,8 @@ def save_export_syndicats(export_sheet, output_file_path):
         export_sheet[row].insert(-2, 2)  # Aire, toujours 2
         for cell in range(len(export_sheet[row])):
             ws.cell(row+2, cell+1).value = export_sheet[row][cell]
-            ws.cell(row+2, cell+1).number_format = '@' # Appliquer le format 'Texte' évite une single quote avant les nombres
+            # Appliquer le format 'Texte' évite une single quote avant les nombres
+            ws.cell(row+2, cell+1).number_format = '@'
     wb.save(output_file_path)
 
 
@@ -117,6 +145,8 @@ def main():
                     empty_cells += 1
                 export_row.append(cell)
             if not empty_cells == row_length:
+                # TODO: vérifier l’input
+                check_row(export_row, xlsx_file, row)
                 export_sheet.append(export_row)
     save_export_syndicats(export_sheet, output_file_path)
 
